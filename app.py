db3ef56bd8f5a0b92ef7aafb1713d3d1dfeab39d
ryaishnavedevi-ops <ryaishnavedevi@gmail.com>
2026-03-21 14:40:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -161,7 +161,6 @@ def index():
 
     return render_template("index.html", result=result, error=error, form_values=form_values)
 
-
 if __name__ == "__main__":
-    import os
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
